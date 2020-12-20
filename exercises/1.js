923b00f5b76597f475cd2abb65acfd357e764859
Arthur Borisenko <artour.borisenko@ya.ru>
2020-12-20 11:08:47 +0300

    initial commit.

---
+++ exercises/1.js
@@ -0,0 +1,9 @@
+function check() {
+    var val = document.getElementById('input-answer').value;
+    if (val == 90) {
+        document.getElementById("correct").style = "display:block"
+    } else {
+        document.getElementById("wrong").style = "display:block"
+    }
+}
+document.getElementById("enter").addEventListener("click",check)
